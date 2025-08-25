9097ab33a00084b9fc7d944d44d762a676d99260
SpreehaJoshi <spr33ha.joshi@gmail.com>
2025-08-25 16:00:56 +0545

    post and joi. Joi doesn't work as import clashes with es6

--- express example/exp-demo.js
+++ express example/exp-demo.js
@@ -1,25 +1,71 @@
-import express from "express"
+import Joi from "joi";
+
+import express, { response } from "express";
 const app = express();
+app.use(express.json()); //middleware
 
 import dotenv from "dotenv"; //esc convention
 dotenv.config();
 const port = process.env.PORT || 8000
 
+const courses = [{ id : 1, name: 'course1'},
+    { id : 2, name: 'course2'},
+    { id : 3, name: 'course3'}
+];
+
 app.get('/', (req, res) =>{
     res.send('Hello World!');
 });
 
-app.get('/api/about', (req, res) =>{
-    res.send([1, 5, 8]);
+app.get('/api/courses', (req, res) =>{
+    res.send(courses);
 });
 
-app.get('/api/about/:id', (req,res)=>{
-    res.send(req.params.id);
-});
+// app.get('/api/courses/:id', (req,res)=>{
+//     res.send(req.params.id);
+// });
 
 //query string params handling
-app.get('/api/posts/:year/:month', (req,res)=>{
+app.get('/api/posts/:year/:month', (req, res)=>{
     res.send(req.query);
 });
+//http://localhost:3000/api/posts/2018/3?sortBy=name -> Query string params: for optional
+
+
+// app.get('/api/courses/:id', (req,res)=>{
+//     const course = courses.find(c => c.id === parseInt(req.params.id)); //req.params.id returns string so need to convernt into int using parseInt
+//     if(!course) res.status(404).send('Course with given ID was not found');
+//     res.send(course);
+// });
+
+app.post('/api/courses', (req, res)=>{
+    const schema = {
+        name: Joy.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    //console.log(result);
+    
+    //INPUT verification
+    // if (!req.body.name || req.body.name.length < 3){
+    //     //400 -> bad request
+    //     res.status(400).send('Name is required and needs to be minimum 3 length');
+    //     return;
+    // }
+
+    if (result.error){
+         //400 -> bad request
+         res.status(400).send(result.error.details[0].message);
+        return;
+     }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course); //returns new obj to client
+});
+
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
